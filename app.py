a5bb07efc9eac11a2c740e7bdb358d7822285670
Luis Filipe <lfcamposcardoso@gmail.com>
2024-06-02 13:46:29 -0300

    subir https

--- app.py
+++ app.py
@@ -23,12 +23,6 @@ cenarios = [
     }
 ]
 
-@app.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace("http://", "https://", 1)
-        return redirect(url, code=301)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
