2c634928d70bc57f684a7fbca1f536a1d31bae0f
Luis Filipe <lfcamposcardoso@gmail.com>
2024-06-06 03:12:22 -0300

    teste

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import time
 from flask import Flask, render_template, redirect, request, url_for, session
+from flask_talisman import Talisman
 import os
 import json
 
@@ -7,37 +7,65 @@ app = Flask(__name__, static_folder='static')
 
 app.secret_key = os.urandom(24)
 
+if 'DYNO' in os.environ:
+    Talisman(app, content_security_policy={
+        'style-src': [
+            '\'self\'',
+            'stackpath.bootstrapcdn.com', 
+        ],
+        'default-src': [
+            '\'self\'',
+            'stackpath.bootstrapcdn.com',
+            'www.googletagmanager.com', 
+        ],
+        'script-src': [
+            '\'self\'',
+            'stackpath.bootstrapcdn.com',
+            'www.googletagmanager.com',
+            'unsafe-inline',  # Use com cautela, se necessário
+        ]
+    })
+
 with open(os.path.join(app.static_folder + '/json', 'cenarios.json'), 'r', encoding='utf-8') as f:
     cenarios = json.load(f)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        session.clear()
         session['score'] = 0
         session['current_scenario'] = 0
+        session['len'] = len(cenarios)
         return redirect(url_for('jogo'))
     return render_template('index.html')
 
 @app.route('/jogo', methods=['GET', 'POST'])
 def jogo():
-
     if 'score' not in session:
-        return redirect(url_for('index'))
+        return redirect(url_for('index'), code=302)
     
-    current_scenario = session['current_scenario']
-    if current_scenario >= len(cenarios):
-        return redirect(url_for('fim'))
-    scenario = cenarios[current_scenario]
-
+    scenario = cenarios[session['current_scenario']]
     if request.method == 'POST':
+        
         selected_option = request.form['option']
-        impact = scenario.get(selected_option + '_choice_impact', {})
+        if selected_option == 'left':
+            impact = scenario['left_choice_impact']
+        else:
+            impact = scenario['right_choice_impact']
+
         session['score'] += sum(impact.values())
         session['current_scenario'] += 1
-        return redirect(url_for('jogo'))
 
-    return render_template('jogo.html', scenario=scenario, score=session['score'])
+        if session['current_scenario'] >= len(cenarios):
+            final_score = session['score']
+            session.clear()
+            return render_template('fim.html', score=final_score)
+
+    if session['current_scenario'] < len(cenarios):
+        return render_template('jogo.html', scenario=scenario, score=session['score'])
+    else:
+        final_score = session['score']
+        session.clear()
+        return render_template('fim.html', score=final_score)
 
 @app.route('/fim')
 def fim():
