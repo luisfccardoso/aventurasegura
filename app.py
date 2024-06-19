b0a3580272753ffabcca7bf6bc962cdb0b079edc
Luis Filipe <lfcamposcardoso@gmail.com>
2024-06-19 11:26:21 -0300

    remover clicky e adicionar evento google analytics

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def add_nonce(response):
     if hasattr(g, 'nonce'):
         csp = (
             "default-src 'self'; "
-            f"script-src 'self' 'nonce-{g.nonce}' www.googletagmanager.com www.google-analytics.com static.getclicky.com getclicky.com in.getclicky.com; "
-            "img-src 'self' www.google-analytics.com getclicky.com; "
-            "connect-src 'self' www.google-analytics.com getclicky.com; "
+            f"script-src 'self' 'nonce-{g.nonce}' www.googletagmanager.com www.google-analytics.com; "
+            "img-src 'self' www.google-analytics.com; "
+            "connect-src 'self' www.google-analytics.com; "
             "style-src 'self' 'unsafe-inline' stackpath.bootstrapcdn.com; "
         )
         response.headers['Content-Security-Policy'] = csp
