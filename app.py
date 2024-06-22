9600295a2b49c1e79099bd7bcfe0665db2e9c92a
Luis Filipe <lfcamposcardoso@gmail.com>
2024-06-21 23:47:16 -0300

    resolvendo problema do primeiro cenário aparecer duas vezes e limitar 10 cenários

--- app.py
+++ app.py
@@ -34,13 +34,15 @@ if 'DYNO' in os.environ:
 with open(os.path.join(app.static_folder + '/json', 'cenarios.json'), 'r', encoding='utf-8') as f:
     cenarios = json.load(f)
 
+cenarios = cenarios[:10] #limitar 10
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     nonce = g.get('nonce', '')
     if request.method == 'POST':
         session['score'] = 0
         session['current_scenario'] = 0
-        session['len'] = len(cenarios)
+        session['len'] = min(len(cenarios), 10)
         return redirect(url_for('jogo'))
     return render_template('index.html', nonce=nonce)
 
@@ -50,11 +52,16 @@ def jogo():
     if 'score' not in session:
         return redirect(url_for('index'), code=302)
     
-    scenario = cenarios[session['current_scenario']]
     cenario_numero = session['current_scenario']
 
+    if cenario_numero >= len(cenarios):
+        final_score = session['score']
+        session.clear()
+        return render_template('fim.html', score=final_score, nonce=nonce, cenario_numero=cenario_numero)
+
+    scenario = cenarios[cenario_numero]
+
     if request.method == 'POST':
-        
         selected_option = request.form['option']
         if selected_option == 'left':
             impact = scenario['left_choice_impact']
@@ -64,17 +71,9 @@ def jogo():
         session['score'] += sum(impact.values())
         session['current_scenario'] += 1
 
-        if session['current_scenario'] >= len(cenarios):
-            final_score = session['score']
-            session.clear()
-            return render_template('fim.html', score=final_score, nonce=nonce, cenario_numero=cenario_numero)
+        return redirect(url_for('jogo'))
 
-    if session['current_scenario'] < len(cenarios):
-        return render_template('jogo.html', scenario=scenario, score=session['score'], nonce=nonce, cenario_numero=cenario_numero)
-    else:
-        final_score = session['score']
-        session.clear()
-        return render_template('fim.html', score=final_score, nonce=nonce, cenario_numero=cenario_numero)
+    return render_template('jogo.html', scenario=scenario, score=session['score'], nonce=nonce, cenario_numero=cenario_numero)
 
 @app.route('/fim')
 def fim():
