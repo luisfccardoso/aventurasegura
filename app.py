0db1f583f4b419a6cd4b97ddde69264f44179c1e
Luis Filipe <lfcamposcardoso@gmail.com>
2024-07-09 19:45:59 -0300

    voltando commit, pois o anterior estava com erro

--- app.py
+++ app.py
@@ -38,7 +38,8 @@ with open(os.path.join(app.static_folder + '/json', 'perfis.json'), 'r', encodin
     perfis = json.load(f)
 
 def get_cenario(cenarios):
-    cenario = cenarios[session['cenario_atual']]
+    
+    cenario = cenarios[session['cenario_atual'] ]
 
     session['id'] = cenario['id']
     session['personagem'] = cenario['personagem']
@@ -65,11 +66,22 @@ def index():
     return render_template(f'index_{versao}.html', nonce=nonce)
 
 
+@app.route('/historia', methods=['GET', 'POST'])
+def historia():
+    nonce = g.get('nonce', '')  
+    session.clear()
+    session['cenario_atual'] = randint(1, 39)
+    get_cenario(cenarios)
+      
+    if request.method == 'POST':
+        return redirect(url_for('jogos'))
+    return render_template('historia.html', nonce=nonce)
+
 @app.route('/historia_a', methods=['GET', 'POST'])
 def historia_a():
     nonce = g.get('nonce', '')  
     session.clear()
-    session['cenario_atual'] = randint(1, 30)
+    session['cenario_atual'] = randint(1, 39)
     get_cenario(cenarios)
       
     if request.method == 'POST':
@@ -80,7 +92,7 @@ def historia_a():
 def historia_b():
     nonce = g.get('nonce', '')  
     session.clear()
-    session['cenario_atual'] = randint(1, 30)
+    session['cenario_atual'] = randint(1, 39)
     get_cenario(cenarios)
       
     if request.method == 'POST':
@@ -123,8 +135,10 @@ def jogos():
                 return render_template('consequencia.html', nonce=nonce, cenario_numero=session['cenarios']-1,mensagem_feedback=session['consequencia_direita'],titulo_feedback="Tente Novamente!")
 
     session['pontuacao'] = 0
-    session['cenario_atual'] = randint(1, 30)
+    session['cenario_atual'] = randint(1,38)
+    print(session['cenario_atual'] )
     session['cenarios'] = 1
+    #get_cenario(cenarios)  # teste teste
 
     return render_template('jogo.html', texto_escolha_direita=session['texto_escolha_direita'], texto_escolha_esquerda=session['texto_escolha_esquerda'], texto=session['texto'], imagem=session['imagem'], personagem=session['personagem'], pontuacao=session['pontuacao'], nonce=nonce, cenario_numero=session['cenarios'])
 
@@ -136,17 +150,29 @@ def consequencia():
 @app.route('/jogo', methods=['GET', 'POST'])
 def jogo():
     nonce = g.get('nonce', '')
-    get_cenario(cenarios)
+    #get_cenario(cenarios)
+    while True:  # Loop para repetir até obter sucesso
+        try:
+            get_cenario(cenarios)
+            break  # Sai do loop se não houver erro
+        except (KeyError, IndexError) as e:  # Captura erros comuns
+            print(f"Erro ao obter cenário: {e}")
+            session['cenarios'] -= 1
 
-    if 'pontuacao' not in session:
-        return redirect(url_for('historia'), code=302)
+    #if 'pontuacao' not in session:
+    #    return redirect(url_for('historia'), code=302)
     
-    if session['cenarios'] > 10:
-        for contador in range(11):
+    if session['cenarios'] > 9:
+        for contador in range(10):
             if session['pontuacao'] == contador:
                 perfil = perfis[contador]
                 break
-        return render_template('fim.html', pontuacao=session['pontuacao'], nonce=nonce, perfil=perfil)
+        # Escolhe aleatoriamente entre as versões A e B
+        versao = choice(['c', 'd'])
+        session['versao_index'] = versao  # Armazena a versão na sessão
+
+   
+        return render_template(f'fim_{versao}.html', pontuacao=session['pontuacao'], nonce=nonce, perfil=perfil)
 
     if request.method == 'POST':
         if request.form['opcao'] == 'esquerda':
@@ -176,11 +202,11 @@ def jogo():
                 session['cenarios'] += 1
                 
                 return render_template('consequencia.html', nonce=nonce, cenario_numero=session['cenarios']-1,mensagem_feedback=session['consequencia_direita'],titulo_feedback="Tente Novamente!")
-        
+    print(session['cenarios'] )    
     return render_template('jogo.html', texto_escolha_direita=session['texto_escolha_direita'], texto_escolha_esquerda=session['texto_escolha_esquerda'], texto=session['texto'], imagem=session['imagem'], personagem=session['personagem'], pontuacao=session['pontuacao'], nonce=nonce, cenario_numero=session['cenarios'])
 
-@app.route('/fim')
-def fim():
+@app.route('/fim_c')
+def fim_c():
     nonce = g.get('nonce', '')
 
     for contador in range(11):
@@ -188,12 +214,34 @@ def fim():
             perfil = perfis[contador]
             break
 
-    return render_template('fim.html', pontuacao=session['pontuacao'], nonce=nonce, perfil=perfil)
+    return render_template('fim_c.html', pontuacao=session['pontuacao'], nonce=nonce, perfil=perfil)
+
+@app.route('/fim_d')
+def fim_d():
+    nonce = g.get('nonce', '')
+
+    for contador in range(10):
+        if session['pontuacao'] == contador:
+            perfil = perfis[contador]
+            break
+
+    return render_template('fim_d.html', pontuacao=session['pontuacao'], nonce=nonce, perfil=perfil)
 
 @app.route('/obrigado')
 def obrigado():
     nonce = g.get('nonce', '')
     return render_template('obrigado.html', nonce=nonce)
+
+@app.route('/obrigado_d')
+def obrigado_d():
+    nonce = g.get('nonce', '')
+    for contador in range(10):
+        if session['pontuacao'] == contador:
+            contador +=1
+            perfil = perfis[contador]
+            
+            break
+    return render_template('obrigado_d.html', pontuacao=contador, nonce=nonce, perfil=perfil)
 
 if __name__ == '__main__':
     app.run()
